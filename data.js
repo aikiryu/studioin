ca17ac57e5e8b23f233394d5f44ce6462a70daae
Aiki <aikiryu1228@naver.com>
2023-03-04 17:17:45 +0900

    add thumbnail img

--- data.js
+++ data.js
@@ -2,27 +2,32 @@ let data = [
     {
         name : '동의동락 온택트 프로그램',
         describtion : '동의대학교 지역봉사 프로젝트',
-        link : '8tWsh9IuSiY'
+        link : '8tWsh9IuSiY',
+        image: 'vid1.jpg'
     },
     {
         name : '논문 cg',
         describtion : 'TV 프로그램 내 CG',
-        link : 'I-9CN3K7MIs'
+        link : 'I-9CN3K7MIs',
+        image: 'vid1.jpg'
     },
     {
         name : '라보엠 SB',
         describtion : '오페라-라보엠 홍보 SB',
-        link : 'S_kqKUPxhHU'
+        link : 'S_kqKUPxhHU',
+        image: 'vid1.jpg'
     },
     {
         name : '아라 쇼츠',
         describtion : '기업(아라F&D)홍보영상',
-        link : 'U9iFYgINOZo'
+        link : 'U9iFYgINOZo',
+        image: 'vid1.jpg'
     },
     {
         name : '아라 홍보영상',
         describtion : '기업(아라F&D)홍보영상',
-        link : 'JYm-jDtiovc'
+        link : 'JYm-jDtiovc',
+        image: 'vid1.jpg'
     },
 ];
 
@@ -36,6 +41,11 @@ data.forEach(function(a,b){
     let p = document.createElement('p');
     let btn = document.createElement('button');
     let span = document.createElement('span');
+    let img = document.createElement('img');
+
+    img.src = `images/${a.image}`;
+    figure.append(img);
+
     item.classList.add('item');
     item.append(figure);
     h4.innerText = a.name;
@@ -47,7 +57,7 @@ data.forEach(function(a,b){
     figcaption.append(p);
     figcaption.append(btn);
     item.append(figcaption);
-
+    
     container.append(item);
 
 });
